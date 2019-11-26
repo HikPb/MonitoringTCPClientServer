8a0010d39cce1c3fc49b6e1f85ad7fef899bb935
HikPb <p.ngocba@gmail.com>
2019-11-26 18:48:18 +0700

    add makefile

---
+++ setup.sh
@@ -0,0 +1,20 @@
+if [ ! -d "images" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+	mkdir images
+fi
+if [ ! -d "info" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir  info 
+fi
+if [ ! -d "processing" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir processing 
+fi
+if [ ! -d "result" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir result 
+fi
+if [ ! -d "logs" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir logs
+fi
